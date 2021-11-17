ffeeed6b0a1decdebd5506343fc81e328ea8909c
adijaay <adiwijayaswastama@gmail.com>
2021-11-17 13:42:25 +0700

    change class
    add main method and plus superclass ATM

---
+++ ATMKOMP/src/ATM.java
@@ -0,0 +1,33 @@
+package bank;
+
+class ATM extends javax.swing.JFrame{
+
+    int masukan;
+    double hasil, saldo;
+    String jawab, username="lanaajaboleh", pass="1234";
+
+    public void setHasil(double hasil) {
+        this.hasil = hasil;
+    }
+
+    public void setMasukan(int masukan) {
+        this.masukan = masukan;
+    }
+
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getHasil() {
+        return hasil;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public int getMasukan() {
+        return masukan;
+    }
+}
